875591f7697fdd7316265c3f55ba7124418f602d
Ben Davenport-Ray <laserdude11@gmail.com>
2011-07-13 15:53:09 -0400

    Restructured so it is actually capable of making a library.

---
+++ include/Battle.h
@@ -0,0 +1,47 @@
+/*
+ * Battle.h
+ *
+ *  Created on: Jul 5, 2011
+ *      Author: ben
+ */
+
+#ifndef BATTLE_H_
+#define BATTLE_H_
+
+#include "Trainer.h"
+#include "Pokemon.h"
+
+
+
+enum Weather{
+	NoWeather = 0,
+	Rain,
+	Sun,
+	Sandstorm,
+	Hail
+};
+
+class Battle {
+private:
+	Trainer* trainer1;
+	Trainer* trainer2;
+	Pokemon* t1poke;
+	Pokemon* t2poke;
+	Move* currentMove;
+
+
+	bool hasEnded;
+
+	void executeTurn();
+
+public:
+	Battle(Trainer* player, Trainer* challenger);
+	virtual ~Battle();
+	Weather currentWeather;
+	int CalculateDamage(Move* move, Pokemon* target);
+	void Begin();
+	void Loop();
+
+};
+
+#endif /* BATTLE_H_ */
